bc6259996989f18669c67036fab78cdba93afdf6
sausage303 <parinya24dev@gmail.com>
2025-02-19 10:00:16 +0700

    fix: update version to 1.0.2 and add autoObjective parameter to Scoreboard constructor

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import * as mc from "@minecraft/server";
 export class Scoreboard<T extends string> {
     public scoreboard: mc.Scoreboard;
     private readonly unzero: boolean;
+    private readonly autoObjective: boolean;
 
-    constructor(unzero: boolean) {
-        this.unzero = unzero;
+    constructor(unzero: boolean = false, autoObjective: boolean = false) {
         this.scoreboard = mc.world.scoreboard;
+        this.unzero = unzero;
+        this.autoObjective = autoObjective;
     }
 
     private zero(value: number) {
@@ -15,16 +17,24 @@ export class Scoreboard<T extends string> {
 
     public get(name: T, player: mc.Player) {
         const objective = this.scoreboard.getObjective(name);
-        if (!objective)
-            this.scoreboard.addObjective(name);
+        if (!objective) {
+            if (this.autoObjective)
+                this.scoreboard.addObjective(name);
+            else
+                throw new Error(`Objective ${name} does not exist`);
+        }
 
         return objective.getScore(player);
     }
 
     public set(name: T, player: mc.Player, value: number) {
         const objective = this.scoreboard.getObjective(name);
-        if (!objective)
-            this.scoreboard.addObjective(name);
+        if (!objective) {
+            if (this.autoObjective)
+                this.scoreboard.addObjective(name);
+            else
+                throw new Error(`Objective ${name} does not exist`);
+        }
 
         objective.setScore(player, this.zero(value));
         return this;
